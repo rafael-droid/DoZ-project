f18d4d80e36b29f1cc87b3da6f3c03f5a4a85d93
s21290 <s21290@pjwstk.edu.pl>
2024-04-18 20:29:11 +0200

    Commit initial

---
+++ DoZ-project/src/main/java/pl/doz/DoZproject/DoZProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.doz.DoZproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoZProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoZProjectApplication.class, args);
+	}
+
+}
---
+++ DoZ-project/src/test/java/pl/doz/DoZproject/DoZProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.doz.DoZproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoZProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
