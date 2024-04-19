a0ecae9fb48a381e437499fc887af2942d3556cb
s21290 <s21290@pjwstk.edu.pl>
2024-04-19 10:45:08 +0200

    Added swagger

--- DoZ-project/src/main/java/pl/doz/DoZproject/DoZProjectApplication.java
+++ DoZ-project/src/main/java/pl/doz/DoZproject/DoZProjectApplication.java
@@ -3,6 +3,7 @@ package pl.doz.DoZproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DoZProjectApplication {
 
